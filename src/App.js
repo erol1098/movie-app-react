2cd7eff7c4e6bb0bba01e14bad474f10dc813b3f
erol1098 <erol1098@gmail.com>
2022-07-21 01:07:40 +0300

    Font Changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-
 import AppRouter from "./routes/AppRouter";
-// import styles from "./App.module.css"
-
 const App = () => {
   return <AppRouter />;
 };
-
 export default App;
