6ea16b768d60dfc82405e55807bf007e3231fea3
erol1098 <erol1098@gmail.com>
2022-07-20 19:00:08 +0300

    Auto logout added.

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -7,29 +7,39 @@ import Details from "../pages/Details/Details";
 import NotFound from "../pages/NotFound/NotFound";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
 import { Navigate } from "react-router-dom";
+import { authActions } from "../store/auth-slicer";
 const AppRouter = () => {
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {isLoggedIn && <Route path="search" element={<Search />} />}
-        {!isLoggedIn && <Route path="search" element={<Login />} />}
-        {isLoggedIn && <Route path="details/:id" element={<Details />} />}
-        {!isLoggedIn && <Route path="details/:id" element={<Login />} />}
-        {!isLoggedIn && <Route path="login" element={<Login />} />}
-        {isLoggedIn && <Route path="login" element={<Navigate to={"/"} />} />}
-        {!isLoggedIn && <Route path="register" element={<Register />} />}
-        <Route path="register" element={<Register />} />
-        {isLoggedIn && (
-          <Route path="register" element={<Navigate to={"/"} />} />
-        )}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <div
+      onClick={() =>
+        localStorage.getItem("willExpire") <= new Date().getTime() &&
+        dispatch(authActions.logout())
+      }
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {isLoggedIn && <Route path="search" element={<Search />} />}
+          {!isLoggedIn && <Route path="search" element={<Login />} />}
+          {isLoggedIn && <Route path="details/:id" element={<Details />} />}
+          {!isLoggedIn && <Route path="details/:id" element={<Login />} />}
+          {!isLoggedIn && <Route path="login" element={<Login />} />}
+          {isLoggedIn && <Route path="login" element={<Navigate to={"/"} />} />}
+          {!isLoggedIn && <Route path="register" element={<Register />} />}
+          <Route path="register" element={<Register />} />
+          {isLoggedIn && (
+            <Route path="register" element={<Navigate to={"/"} />} />
+          )}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
